93ce6f420cc362961263301d3dcb798d8970f031
Yeiber Cano <yeibercano@gmail.com>
2016-07-27 20:18:37 -0700

    render homepage and item_detail while handling 404

--- inventory/views.py
+++ inventory/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
 
-from django.http import HttpResponse
+from inventory.models import Item
+
 
 def index(request):
-	return HttpResponse('<p>In index view</p>')
+	items = Item.objects.exclude(amount=0)
+	return render(request, 'inventory/index.html', {
+		'items': items,
+	})
 
 def item_detail(request, id):
-	return HttpResponse('<p>In item_detail view with id{0}</p>'.format(id))
+	try:
+		item = Item.objects.get(id=id)
+	except Item.DoesNotExist:
+		raise Http404('This item does not exist')
+	return render(request, 'Inventory/item_detail.html', {
+		'item': item,
+	})
+	
