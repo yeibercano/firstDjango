94dce6a92f76a6ccc4e96d03d2956dfc50344a5c
Yeiber Cano <yeibercano@gmail.com>
2016-07-27 18:08:42 -0700

    connect url patterns with views

--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'firstdjango.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from inventory import views
 
+urlpatterns = [
+		url(r'^$', views.index, name='index'),
+		url(r'^item/(?P<id>\d+)/', views.item_detail, name='item_detail'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse('<p>In index view</p>')
+
+def item_detail(request, id):
+	return HttpResponse('<p>In item_detail view with id{0}</p>'.format(id))
